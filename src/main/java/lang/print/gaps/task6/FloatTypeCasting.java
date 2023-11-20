baf29f7526c3e6faf82394b9920ead8723b12c78
ZooM <yuhos@mail.ru>
2023-11-20 22:02:45 +0300

    All brackets on their places

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,12 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        double remainder = numberToBeRounded - (int) numberToBeRounded;
+        if (remainder >= 0.5) {
+            numberToBeRounded = (int) numberToBeRounded + 1;
+            System.out.println((int) numberToBeRounded);
+        } else {
+            System.out.println((int) numberToBeRounded);
+        }
     }
 }
